d6018ac5a42dc8e7a7b2d41bbb99a400ce1466eb
Matt Middleton <mmiddleton3301@outlook.com>
2017-05-28 09:31:08 +0100

    Polishing up/cleaning up concrete implementations.

--- Meridian.InterSproc/Meridian.InterSproc/ContractMethodInformationConverter.cs
+++ Meridian.InterSproc/Meridian.InterSproc/ContractMethodInformationConverter.cs
@@ -1,4 +1,13 @@
-﻿namespace Meridian.InterSproc
+﻿// ----------------------------------------------------------------------------
+// <copyright
+//      file="ContractMethodInformationConverter.cs"
+//      company="MTCS (Matt Middleton)">
+// Copyright (c) Meridian Technology Consulting Services (Matt Middleton).
+// All rights reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+
+namespace Meridian.InterSproc
 {
     using System;
     using System.Collections.Generic;
@@ -7,11 +16,29 @@
     using Meridian.InterSproc.Definitions;
     using Meridian.InterSproc.Model;
 
-    public class ContractMethodInformationConverter : IContractMethodInformationConverter
+    /// <summary>
+    /// Implements <see cref="IContractMethodInformationConverter" />.
+    /// </summary>
+    public class ContractMethodInformationConverter
+        : IContractMethodInformationConverter
     {
+        /// <summary>
+        /// The default schema for SQL server databases.
+        /// </summary>
         private const string DefaultSchema = "dbo";
 
-        public ContractMethodInformation[] GetContractMethodInformationFromContract<DatabaseContractType>() where DatabaseContractType : class
+        /// <summary>
+        /// Implements
+        /// <see cref="IContractMethodInformationConverter.GetContractMethodInformationFromContract{DatabaseContractType}()" />.
+        /// </summary>
+        /// <typeparam name="DatabaseContractType">
+        /// The database contract interface type.
+        /// </typeparam>
+        /// <returns>
+        /// An array of <see cref="ContractMethodInformation" /> instances. 
+        /// </returns>
+        public ContractMethodInformation[] GetContractMethodInformationFromContract<DatabaseContractType>()
+            where DatabaseContractType : class
         {
             ContractMethodInformation[] toReturn = null;
 
@@ -35,6 +62,66 @@
             return toReturn;
         }
 
+        /// <summary>
+        /// Ammends an instance of <see cref="ContractMethodInformation" />
+        /// with information based on the input
+        /// <see cref="CustomAttributeData" />.
+        /// </summary>
+        /// <param name="contractMethodInformation">
+        /// An instance of <see cref="ContractMethodInformation" />.
+        /// </param>
+        /// <param name="customAttributeData">
+        /// An instance of <see cref="CustomAttributeData" /> that may or may
+        /// not contain data to ammend the input
+        /// <paramref name="contractMethodInformation" />. 
+        /// </param>
+        private void AmmendContractMethodInformationWithAttributeData(
+            ContractMethodInformation contractMethodInformation,
+            CustomAttributeData customAttributeData)
+        {
+            this.ExtractCustomAttributeValue(
+                customAttributeData,
+                nameof(InterSprocContractAttribute.Schema),
+                x =>
+                {
+                    contractMethodInformation.Schema = x;
+                });
+
+            this.ExtractCustomAttributeValue(
+                customAttributeData,
+                nameof(InterSprocContractAttribute.Prefix),
+                x =>
+                {
+                    contractMethodInformation.Prefix = x;
+                });
+
+            this.ExtractCustomAttributeValue(
+                customAttributeData,
+                nameof(InterSprocContractMethodAttribute.Name),
+                x =>
+                {
+                    contractMethodInformation.Name = x;
+                });
+        }
+
+        /// <summary>
+        /// Composes a <see cref="ContractMethodInformation" /> instance
+        /// according to the naming priorities of:
+        /// a) Method level attributes;
+        /// b) Interface level attributes;
+        /// c) Information extracted from the name of the method.
+        /// </summary>
+        /// <param name="classLevelAttribute">
+        /// The class-level <see cref="ContractMethodInformation" /> instance
+        /// (if there is one).
+        /// </param>
+        /// <param name="methodInfo">
+        /// A <see cref="MethodInfo" /> instance describing the current method
+        /// being converted.
+        /// </param>
+        /// <returns>
+        /// An instance of <see cref="ContractMethodInformation" />. 
+        /// </returns>
         private ContractMethodInformation ConvertMethodInfoToContractMethodInformation(
             CustomAttributeData classLevelAttribute,
             MethodInfo methodInfo)
@@ -87,35 +174,24 @@
             return toReturn;
         }
 
-        private void AmmendContractMethodInformationWithAttributeData(
-            ContractMethodInformation contractMethodInformation,
-            CustomAttributeData customAttributeData)
-        {
-            this.ExtractCustomAttributeValue(
-                customAttributeData,
-                nameof(InterSprocContractAttribute.Schema),
-                x =>
-                {
-                    contractMethodInformation.Schema = x;
-                });
-
-            this.ExtractCustomAttributeValue(
-                customAttributeData,
-                nameof(InterSprocContractAttribute.Prefix),
-                x =>
-                {
-                    contractMethodInformation.Prefix = x;
-                });
-
-            this.ExtractCustomAttributeValue(
-                customAttributeData,
-                nameof(InterSprocContractMethodAttribute.Name),
-                x =>
-                {
-                    contractMethodInformation.Name = x;
-                });
-        }
-
+        /// <summary>
+        /// Pulls back a <see cref="string" /> value from the input
+        /// <paramref name="customAttributeData" /> with a particular name
+        /// (<paramref name="valueName"/>).
+        /// If not null, then <paramref name="setMethod" /> is executed,
+        /// injecting the extracted <see cref="string" /> value.
+        /// </summary>
+        /// <param name="customAttributeData">
+        /// An instance of <see cref="CustomAttributeData" /> to extract values
+        /// from.
+        /// </param>
+        /// <param name="valueName">
+        /// The name of the value to extract.
+        /// </param>
+        /// <param name="setMethod">
+        /// An instance of <see cref="Action{string}" /> which is invoked if
+        /// the value extracted is not null. 
+        /// </param>
         private void ExtractCustomAttributeValue(
             CustomAttributeData customAttributeData,
             string valueName,
