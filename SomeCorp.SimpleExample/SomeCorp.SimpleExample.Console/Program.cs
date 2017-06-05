1fa15aef1a21c5ad96bc61273388e9d449ca1b5f
Matt Middleton <mmiddleton3301@outlook.com>
2017-06-05 09:23:45 +0100

    Example project - console app project founded.

---
+++ SomeCorp.SimpleExample/SomeCorp.SimpleExample.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="Program.cs" company="MTCS (Matt Middleton)">
+// Copyright (c) Meridian Technology Consulting Services (Matt Middleton).
+// All rights reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+
+namespace SomeCorp.SimpleExample.Console
+{
+    /// <summary>
+    /// Contains the main entry point to the application,
+    /// <see cref="Program.Main()" />. 
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The main entry point to the application.
+        /// </summary>
+        private static void Main()
+        {
+        }
+    }
+}
---
+++ SomeCorp.SimpleExample/SomeCorp.SimpleExample.Console/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="MTCS (Matt Middleton)">
+// Copyright (c) Meridian Technology Consulting Services (Matt Middleton).
+// All rights reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Assembly info.
+[assembly: AssemblyTitle("SomeCorp SimpleExample")]
+[assembly: AssemblyDescription("An example project written to provide a working example of the InterSproc library.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Meridian Technology Consulting Services (Matt Middleton)")]
+[assembly: AssemblyProduct("Meridian InterSproc")]
+[assembly: AssemblyCopyright("Copyright (c) Meridian Technology Consulting Services (Matt Middleton)")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("54325484-aae1-4b4c-ae84-4c42a574c7f4")]
+
+// Version info.
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
